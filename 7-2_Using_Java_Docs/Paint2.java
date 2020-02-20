5aa30b1173b4cc016a6338ea4320a2d897b336d3
Joel Guerra <joel.guerra@walmartlabs.com>
2020-02-19 21:10:53 -0800

    completes the assignment

---
+++ 7-2_Using_Java_Docs/Paint2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Paint2 {
+
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+        double wallHeight = 0.0;
+        double wallWidth = 0.0;
+        double wallArea = 0.0;
+        double gallonsPaintNeeded = 0.0;
+        double cansNeeded;
+
+        final double squareFeetPerGallons = 350.0;
+        // final double gallonsPerCan = 1.0; This variable is not needed.
+
+        // Prompt user to input wall's height
+        System.out.println("Enter wall height (feet): ");
+        wallHeight = scnr.nextDouble();
+
+
+        // Prompt user to input wall's width
+        System.out.println("Enter wall width (feet): ");
+        wallWidth = scnr.nextDouble();
+
+        // Calculate and output wall area
+        wallArea = wallHeight * wallWidth;
+        System.out.println("Wall area:  " + wallArea + " square feet");
+
+        // Calculate and output the amount of paint in gallons needed to paint the wall
+        gallonsPaintNeeded = wallArea / squareFeetPerGallons;
+        System.out.println("Paint needed: " + gallonsPaintNeeded + " gallons");
+
+        // Calculate and output the number of paint cans needed to paint the wall,
+        // rounded up to nearest integer
+        // Complete this code block
+        cansNeeded = Math.ceil(gallonsPaintNeeded);
+        System.out.println("Cans needed: " + cansNeeded + " can(s)");
+
+    }
+}
